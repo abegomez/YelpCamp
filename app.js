3dd8b23ebead4983d87fdc70472844b30c69dab7
agomez717 <agomez717@gmail.com>
2019-01-02 07:56:34 +0000

    change to mongodb on heroku

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
+// mongoose.connect("mongodb://localhost/yelp_camp_v11");
 mongoose.connect("mongodb://localhost/yelp_camp_v11");
-
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
